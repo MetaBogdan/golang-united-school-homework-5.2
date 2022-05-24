a88467f755147c3df4816bfefedcab0d16136e73
Bogdan Vozniy <baglandir@gmail.com>
2022-05-24 16:03:35 +0800

    first

--- cache.go
+++ cache.go
@@ -3,21 +3,51 @@ package cache
 import "time"
 
 type Cache struct {
+	massivCash map[string]saveVal
+}
+
+type saveVal struct {
+	value  string
+	record time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{massivCash: make(map[string]saveVal)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (in *Chash) Get(key string) (string, bool) {
+	if value, ok := in.massivCash[key]; ok {
+		if value.record.IsZero() || time.Now().Before(value.record) {
+
+			return value.value, true
+		}
+		delete(in.massivCash, key)
+	}
+	return "", false
+}
+
+func (in *Chash) Put(key, value string) {
+
+	in.massivCash[key] = saveVal{
+		value:  value,
+		record: time.Time{},
+	}
+}
+
+func (in *Chash) Keys() []string {
+	listKeys := make([]string, 0)
+	for k := range in.massivCash {
+		listKeys = append(listKeys, k)
+
+	}
+	return listKeys
 
 }
 
-func (receiver) Put(key, value string) {
-}
+func (in *Chash) PutTill(key, value string, deadline time.Time) {
+	in.massivCash[key] = saveVal{
+		value:  value,
+		record: deadline,
+	}
 
-func (receiver) Keys() []string {
-}
-
-func (receiver) PutTill(key, value string, deadline time.Time) {
 }
